a75141048277995f1399875993c29cf30505359c
dev-montgomery <montgomery.richard.dev@gmail.com>
2025-01-23 20:30:26 -0800

    player moves and updates position in the world.

--- src/class.js
+++ src/class.js
@@ -1,14 +1,17 @@
+const screenWidth = 832;
+const screenHeight = 704;
+
 export class Player {
   constructor() {
     this.image = new Image();
     this.image.src = '../backend/assets/player_data/assets-player.png';
     this.pixels = 64;
-    this.speed = 1;
-    this.direction = 'down';
+    this.speed = 0.5;
+    this.direction = { x: 0, y: 128 };
     this.cooldown = false;
     this.drawTo = { 
-      x: window.innerWidth * 0.5 - 128,
-      y: window.innerHeight * 0.5 - 128 
+      x: (screenWidth / 2) - (this.pixels / 2 + this.pixels),
+      y: (screenHeight / 2) - (this.pixels / 2 + this.pixels)
     };
   };
 
@@ -17,6 +20,20 @@ export class Player {
       this.image.onload = () => resolve(this.image);
       this.image.onerror = () => reject(new Error('Failed to load player image.'));
     });
+  };
+
+  draw(ctx) {
+    ctx.drawImage(
+      this.image,
+      this.direction.x,
+      this.direction.y,
+      this.pixels + this.pixels,
+      this.pixels + this.pixels,
+      this.drawTo.x,
+      this.drawTo.y,
+      this.pixels + this.pixels,
+      this.pixels + this.pixels
+    );
   };
 };
 
